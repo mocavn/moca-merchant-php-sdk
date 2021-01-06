6ed7f53e2236cddc936d5bb73c06682b57e45df9
phuoc.nguyenthanh <phuoc.nguyenthanh@grabtaxi.com>
2021-01-06 16:49:24 +0700

    update sdk

--- src/MocaTransaction.php
+++ src/MocaTransaction.php
@@ -60,7 +60,8 @@ class MocaTransaction
             if ($resp->code == 200) {
                 $bodyResp = $resp->body;
                 $scope = 'payment.vn.one_time_charge';
-                $codeChallenge = $this->base64URLEncode(base64_encode(hash('sha256', $this->base64URLEncode($this->getPartnerTxID().$this->getPartnerTxID()))));
+                $code_verifier = $this->base64URLEncode($this->getPartnerTxID().$this->getPartnerTxID());
+                $codeChallenge = $this->base64URLEncode(hash('sha256', $code_verifier));
 
                 return MocaRestClient::apiEndpoint() .'/grabid/v1/oauth2/authorize?acr_values=consent_ctx%3AcountryCode%3DVN,currency%3DVND&client_id='.getenv('MOCA_MERCHANT_CLIENT_ID').
                     '&code_challenge='.$codeChallenge.'&code_challenge_method=S256&nonce='.$this->generateRandomString(16).
